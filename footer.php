ce5e0f4e7cf30418810c7bcfef4a81f9eb556826
Patrick Dolan <dolan@Patricks-MacBook-Pro.local>
2019-11-20 09:37:57 +0100

    updated info

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </div><!-- /.container -->
 
 <footer class="blog-footer">
-    <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>Site built for <a href="http://getbootstrap.com">The Park</a> built with love by <a href="https://www.thepark.se">@thepark</a>.</p>
     <p><a href="#">Back to top</a></p>
 </footer>
 <?php wp_footer(); ?>
